9cae46f62b43ae244f134b6b868e9e9763a12328
FatihMedAmine <fatihmohamedamin@gmail.com>
2025-11-17 11:31:30 +0100

    refactor: clean up Book class properties and constructor for better readability

--- exercise-express/types/Book.ts
+++ exercise-express/types/Book.ts
@@ -2,21 +2,50 @@ import { Format } from "./enums/Format";
 import { Status } from "./enums/Status";
 
 class Book {
-    private title: string;
-    private author: string;
-    private numberOfPages: number;
-    private status : Status;
-    private price: number;
-    private numberOfPagesRead = 0;
-    private format: Format;
-    private finished: boolean = false;
+  private title: string;
+  private author: string;
+  private numberOfPages: number;
+  private status: Status;
+  private price: number;
+  private numberOfPagesRead: number;
+  private format: Format;
+  private finished: boolean;
 
-    constructor(title: string, author: string, numberOfPages: number, status: Status, price: number, format: Format) {
-        this.title = title;
-        this.author = author;
-        this.numberOfPages = numberOfPages;
-        this.status = status;
-        this.price = price;
-        this.format = format;
+  constructor(
+    title: string,
+    author: string,
+    numberOfPages: number,
+    status: Status,
+    price: number,
+    format: Format
+  ) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.status = status;
+    this.price = price;
+    this.numberOfPagesRead = 0;
+    this.format = format;
+    this.finished = false;
+  }
+
+  setNumberOfPagesRead(pagesRead: number): void {
+    if (pagesRead >= this.numberOfPages) {
+      this.numberOfPagesRead = this.numberOfPages;
+      this.finished = true;
+      this.status = Status.Read;
+    } else {
+      this.numberOfPagesRead = pagesRead;
     }
+  }
+
+  getCurrentlyAt(): number {
+    return this.numberOfPagesRead;
+  }
+
+  deleteBook(): void {
+    // Implementation for deleting the book can be added here
+  }
 }
+
+export { Book };
